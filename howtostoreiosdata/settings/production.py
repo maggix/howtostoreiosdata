7c38d0ddcb50a1072f9923d8df799b95d252f2be
Erik Romijn <erik@erik.io>
2013-12-01 15:14:19 +0100

    switch to postgresql

--- howtostoreiosdata/settings/production.py
+++ howtostoreiosdata/settings/production.py
@@ -2,13 +2,13 @@ from .base import *
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
-ALLOWED_HOSTS = ('....', )
+ALLOWED_HOSTS = ('.howtostoreiosdata.com', )
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'howtostoreiosdata',
-        'USER': 'howtostoreiosdata',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable("DB_NAME"),
+        'USER': get_env_variable("DB_USERNAME"),
         'PASSWORD': get_env_variable("DB_PASSWORD")
     }
 }
